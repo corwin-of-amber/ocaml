9d77d1c93d76f5e6a82a2f58b8f8781e356464af
Shachar Itzhaky <corwin.amber@gmail.com>
2020-05-24 23:21:50 +0300

    Added README and mkdist.sh.

---
+++ mkdist.sh
@@ -0,0 +1,5 @@
+rm -f dist.zip
+zip -j dist.zip ocaml ocamlc stdlib/*.cm[ai] stdlib/*.cmo otherlibs/str/*.cm[ai] otherlibs/unix/*.cm[ai] otherlibs/systhreads/*.cm[ai]
+
+# Add packages
+zip -j dist.zip ../ocaml-pkgs/num/src/*.cm[ai]
