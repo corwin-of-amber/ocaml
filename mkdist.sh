0f40c89065606b6b5f0b67a2b1a00f13dba6c913
Shachar Itzhaky <corwin.amber@gmail.com>
2020-05-30 13:07:43 +0300

    Packages for NPM.

--- mkdist.sh
+++ mkdist.sh
@@ -1,5 +1,21 @@
-rm -f dist.zip
-zip -j dist.zip ocaml ocamlc stdlib/*.cm[ai] stdlib/*.cmo otherlibs/str/*.cm[ai] otherlibs/unix/*.cm[ai] otherlibs/systhreads/*.cm[ai]
+VER=4.09.2
+export COPYFILE_DISABLE=1
 
-# Add packages
-zip -j dist.zip ../ocaml-pkgs/num/src/*.cm[ai]
+rm -f base.zip
+zip -j base.zip ocaml ocamlc stdlib/*.cm[ai] stdlib/*.cmo otherlibs/str/*.cm[ai] otherlibs/unix/*.cm[ai] otherlibs/systhreads/*.cm[ai]
+
+
+rm -rf package 
+mkdir -p package/bin
+cp *.wasm runtime/ocamlrun.wasm package/bin/
+cp package.json README.md package/
+touch package/index.js
+tar zcf ocaml-wasm-$VER.tar.gz package
+
+
+rm -rf package
+mkdir -p package
+cp package.base.json package/package.json
+cp README.md base.zip package/
+touch package/index.js
+tar zcf ocaml-wasm-$VER-base.tar.gz package
