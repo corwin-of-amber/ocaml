e28369def212929e756fb9070e9901769f496fc1
Shachar Itzhaky <corwin.amber@gmail.com>
2020-06-06 20:13:35 +0300

    Packages for NPM & sanity test.

---
+++ mkdist.sh
@@ -0,0 +1,21 @@
+VER=4.10.1
+export COPYFILE_DISABLE=1
+
+rm -f base.zip
+zip -j base.zip ocaml ocamlc stdlib/*.cm[ai] stdlib/*.cmo otherlibs/str/*.cm[ai] otherlibs/unix/*.cm[ai] otherlibs/systhreads/*.cm[ai]
+
+
+rm -rf package 
+mkdir -p package/bin
+cp *.wasm runtime/ocamlrun.wasm package/bin/
+cp package.json README.md package/
+touch package/index.js
+tar zcf ocaml-wasm-$VER.tar.gz package
+
+
+rm -rf package
+mkdir -p package
+cp package.base.json package/package.json
+cp README.md base.zip package/
+touch package/index.js
+tar zcf ocaml-wasm-$VER-base.tar.gz package
