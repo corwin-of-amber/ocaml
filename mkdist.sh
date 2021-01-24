0405ae4d6926c74a0b5512e785b85f2ca33a0d68
Shachar Itzhaky <corwin.amber@gmail.com>
2021-01-25 00:01:37 +0200

    [bugfix] Throw on unimplemented Unix stubs (except a select few).

--- mkdist.sh
+++ mkdist.sh
@@ -1,21 +1,28 @@
-VER=4.10.1
 export COPYFILE_DISABLE=1
 
 rm -f base.zip
 zip -j base.zip ocaml ocamlc stdlib/*.cm[ai] stdlib/*.cmo otherlibs/str/*.cm[ai] otherlibs/unix/*.cm[ai] otherlibs/systhreads/*.cm[ai]
 
 
+VER=`node -p 'require("./package.json").version'`
+FN=ocaml-wasm-$VER.tgz
+
 rm -rf package 
 mkdir -p package/bin
 cp *.wasm runtime/ocamlrun.wasm package/bin/
 cp package.json README.md package/
 touch package/index.js
-tar zcf ocaml-wasm-$VER.tar.gz package
+tar zcf $FN package
+echo $FN
 
+
+VER=`node -p 'require("./package.base.json").version'`
+FN=ocaml-wasm-$VER--base.tgz
 
 rm -rf package
 mkdir -p package
 cp package.base.json package/package.json
 cp README.md base.zip package/
 touch package/index.js
-tar zcf ocaml-wasm-$VER-base.tar.gz package
+tar zcf $FN package
+echo $FN
